af88321c3cc098d5e247ac73c568e97440794a7b
katrinrodkina <offthecliff46@gmail.com>
2019-10-01 19:44:17 -0400

    updated styles

--- bar_chart/index.js
+++ bar_chart/index.js
@@ -1,4 +1,4 @@
-import {select, csv, scaleLinear, max, scaleBand} from 'd3';
+import {select, csv, scaleLinear, max, scaleBand, axisLeft, axisBottom} from 'd3';
 
 const svg = select('svg');
 
@@ -6,23 +6,34 @@ const height = +svg.attr('height');
 const width = parseFloat(svg.attr('width'));
 
 const render = data => {
+  
+  const margin = {top:20, bottom:20, left:100, right:20}
+  const innerWidth = width - margin.left - margin.right
+  const innerHeight = height - margin.top - margin.bottom
+        
   const xScale = scaleLinear()
   	.domain([0,max(data, d => d.population)])
-  	.range([0, width])
+  	.range([0, innerWidth])
+  
   
    const yScale = scaleBand()
   	.domain(data.map( d => d.country))
-  	.range([0, height])
+  	.range([0, innerHeight])
+		.padding(0.1)
   
-  
+  const g = svg.append('g')
+  	.attr('transform', `translate(${margin.left}, ${margin.top})`)
+        
   console.log(xScale.domain())
   console.log(xScale.range())
   console.log(yScale.domain())
   console.log(yScale.range())
 
-
-  
-  svg.selectAll('rect').data(data)
+	g.append('g').call(axisLeft(yScale))
+  g.append('g').call(axisBottom(xScale))
+  	.attr('transform', `translate(${0}, ${innerHeight})`)
+    
+  g.selectAll('rect').data(data)
   	.enter().append('rect')
   	.attr('y', d => yScale(d.country))
   	.attr('width', d => xScale(d.population))
